4daf0ec4a357f16ae3b5b306216f9e703b9908dc
Yahddyyp <Yahddyyp@gmail.com>
2026-04-07 19:12:33 +0530

    Made changes ot fish config for yazi

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -30,3 +30,12 @@ zoxide init fish | source
 if status is-interactive
     atuin init fish | source
 end
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
