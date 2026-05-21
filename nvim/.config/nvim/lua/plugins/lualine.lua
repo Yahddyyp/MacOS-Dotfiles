652e482ae67882a89ee5113db8ae3aa8d9402ca8
Yahddyyp <Yahddyyp@gmail.com>
2026-05-21 21:58:19 +0530

    Lualine update

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,16 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-        opts.options.theme = function()
-            if vim.g.colors_name == "mauve" then
-                return require("mauve-nvim.integrations.lualine")
-            end
-            return "auto"
-        end
-        opts.sections.lualine_z = {
-            function()
-                return " " .. os.date("%I:%M %p")
-            end,
-        }
-    end,
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    opts.options.theme = function()
+      if vim.g.colors_name == "mauve" then
+        return require("mauve.integrations.lualine")
+      end
+      return "auto"
+    end
+    opts.sections.lualine_z = {
+      function()
+        return " " .. os.date("%I:%M %p")
+      end,
+    }
+  end,
 }
