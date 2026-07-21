e7b366b3c02ba12b7652ec02f76c20956e7a2710
Yahddyyp <Yahddyyp@gmail.com>
2026-07-21 18:14:53 +0530

    Disabled spell checking

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,5 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_picker = "telescope"
+vim.opt.spell = false
 vim.opt.splitright = true
 vim.opt.splitbelow = true
