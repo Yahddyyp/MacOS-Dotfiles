8ddcb0504d031877a6692b1a9d4b2f15b92ce966
Yahddyyp <Yahddyyp@gmail.com>
2026-08-08 22:32:10 +0530

    chore: Remove ipv6 addresses

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -10,7 +10,7 @@
   # Cloudflare DNS
   networking = {
     knownNetworkServices = [ "Wi-Fi" ];
-    dns = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+    dns = [ "1.1.1.1" "1.0.0.1" ];
   };
 
   environment.systemPackages = with pkgs; [
