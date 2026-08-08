75ccd3a2c9e167187257a75d9dd10bc8f641f15f
Yahddyyp <Yahddyyp@gmail.com>
2026-08-08 12:41:13 +0530

    feat: Made cloudflare the dns nix-darwin sets up

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -7,6 +7,12 @@
   };
   system.primaryUser = username;
 
+  # Cloudflare DNS
+  networking = {
+    knownNetworkServices = [ "Wi-Fi" ];
+    dns = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+  };
+
   environment.systemPackages = with pkgs; [
     sketchybar
     yabai
