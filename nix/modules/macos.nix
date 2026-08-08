8bfd0ebb77ab94eba7873a6075964c259cf3ca2c
Yahddyyp <Yahddyyp@gmail.com>
2026-08-08 21:08:24 +0530

    feat: Made textedit in a untitled file instead of finder dialog

--- nix/modules/macos.nix
+++ nix/modules/macos.nix
@@ -162,6 +162,9 @@
         TALLogoutSavesState = false;
         SHOWFULLNAME = true;
       };
+      "com.apple.TextEdit" = {
+        NSShowAppCentricOpenPanelInsteadOfUntitledFile = false;
+      };
     };
   };
 
